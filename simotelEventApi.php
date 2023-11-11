e533d2e184495c9b063c4b8a0d838cd70af83bed
hosein <85564616+hoseinalizadeh1373@users.noreply.github.com>
2023-11-11 19:08:39 +0330

    update simoteleventapi

--- simotelEventApi.php
+++ simotelEventApi.php
@@ -9,14 +9,16 @@ use Monolog\Handler\StreamHandler;
 
 $simotel = new Simotel();
 $log = new Log();
+$log->info($_REQUEST["event_name"]);
 
 $simotel->eventApi()->addListener('Ping', function ($data) {
     $log = new Log();
+    $log->info( $_REQUEST['event_name']);
     $logger = new Logger("daily");
     $stream_handler = new StreamHandler(__DIR__ . '/ping.log', Logger::DEBUG);
     // $log->info("Ping",$data);
     $logger->pushHandler($stream_handler);
-    $logger->info($data);
+    $logger->info($_REQUEST['date']);
 });
 $simotel->eventApi()->addListener('NewState', function ($data) {
     $log = new Log();
