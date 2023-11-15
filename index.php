a4196cc442b7e036e0ef4139d36e3a4f726fbbc1
hosein <85564616+hoseinalizadeh1373@users.noreply.github.com>
2023-11-15 18:09:03 +0330

    index

--- index.php
+++ index.php
@@ -2,70 +2,157 @@
 
 require("./vendor/autoload.php");
 require("simotelApi.php");
+require("simotelConfig.php");
 
 ?>
-
 <html>
   <head>
   <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>محتویات فایل لاگ</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-nU14brUcp6StFntEOOEBvcJm4huWjB0OcIeQ3fltAfSmuZFrkAif0T+UtNGlKKQv" crossorigin="anonymous">
-    
-    <script src="
-https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js
-"></script>
-   
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
-  <body class="container" dir="rtl">
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link bg-secondary text-warning mx-1 rounded active" aria-current="page" >ارسال و تست api </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link bg-secondary text-light mx-2 rounded" href="/WebHookApiView.php">نمایش لاگ های دریافتی</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link bg-secondary text-light mx-2 rounded" href="simotelConfig.php">تنظیمات سرور </a>
-        </li>
-       
-      </ul>
+  <body class="" >
+<div class="container">
+  <div class="row">
+  <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h1>Send Request Ping </h1>
+          </div>
+          <div class="card-body">
+          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form id="dataForm" method="post">
+            <div class="mb-3">
+                <label for="serverAddress" class="form-label">آدرس سرور:</label>
+                <input type="text" class="form-control" name="serverAddress" id="serverAddress" required value="<?php echo  isset($data) == true ? $data['simotelApi']['server_address'] : ""?>">
+            </div>
+            <div class="mb-3">
+            <label for="tokenType" class="form-label">نوع احراز هویت:</label>
+                <select class="form-control" id="tokenType" name="tokenType"  required onchange="toggleFields()">
+                
+                    <option value="token" <?php echo(isset($data) == true) && $data['simotelApi']['api_auth'] == "token" ? "selected" : ""?>>token</option>
+                    <option value="basicAuth" <?php echo(isset($data) == true) && $data['simotelApi']['api_auth'] == "basicAuth" ? "selected" : ""?>>basicAuth</option>
+                    <option value="both" <?php echo(isset($data) == true) && $data['simotelApi']['api_auth'] == "both" ? "selected" : ""?>>both</option>
+                    
+                </select>
+            </div>
+            <div class="mb-3" id="div-token">
+                <label for="token" class="form-label" >توکن:</label>
+                <input type="text" class="form-control" name="token" id="token" value="<?php echo  isset($data) == true ? $data['simotelApi']['api_key'] : ""?>" required>
+            </div>
+            <div class="mb-3" id="div-username">
+                <label for="username" class="form-label">نام کاربری:</label>
+                <input type="text" class="form-control" name="username" id="username" value="<?php echo  isset($data) == true ? $data['simotelApi']['api_user'] : ""?>" required>
+            </div>
+            <div class="mb-3" id="div-password">
+                <label for="password" class="form-label">رمز عبور:</label>
+                <input type="password" class="form-control" name="password" id="password" value="<?php echo  isset($data) == true ? $data['simotelApi']['api_pass'] : "" ?>" required>
+            </div>
+           
+            <button type="submit" class="btn btn-primary">ثبت</button>
+        </form>
+
+      </div>
+      
     </div>
   </div>
-</nav>
+</div>
+  
+  <div class="row align-items-center">
+    <div class="col-3">
+    <label for="server_address" class="form-label">simotel server :</label>
+    </div>
+    <div class="col-6">
+    <input type="text" class="form-control" name="username" id="username" value="<?php echo  isset($data) == true ? $data['simotelApi']['server_address'] : ""?>" required>
+    </div>
+    <div class="col-1">
+    <button type="button" class="btn " id="btnsetting" onclick="toggleFields()" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    <i class="bi bi-gear"></i>
+    </button>
+    </div>
+  </div>
+  <div class="row align-items-center mt-2">
+    <div class="col-4">
+      <button class="btn btn-info" id="btn_send_ping">send request<i class="m-2 bi bi-send" ></i></button>
+    </div>
+    <div class="col-3">
+      <span id="status_ping"> </span>
+    </div>
+  </div>
   <table id="logTable" class="table">
             <thead>
                 <tr>
-                    <th>ردیف</th>
-                    <th>محتوا</th>
+                    <th>تاریخ</th>
+                    <th>زمان</th>
+                    <th>وضعیت</th>
+                    <th>پیام</th>
+                    <th>دیتا</th>  
+                  </tr>
+            </thead>
+            <tbody></tbody>
+        </table>
+  </div>
+          </div>
+        </div>
+     
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h1>Reply From Server</h1>
+          </div>
+          <div class="card-body">
+          <table id="pingTable" class="table">
+            <thead>
+                <tr>
+                <th>تاریخ</th>
+                <th>زمان</th>
+                    <th>وضعیت</th>
+                    <th>پیام</th>
+                    <th>دیتا</th>  
                 </tr>
             </thead>
             <tbody></tbody>
         </table>
-  
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<script src="/style/js/index.js"></script>
+<script src="/style/js/webhook.js"></script>
 <script>
 $(document).ready(function() {
   
-    updateLogTable(); // برای بارگذاری اولیه جدول
+    toggleFields(); // فراخوانی اولیه تابع toggleFields
+
+     updateLogTable(); // برای بارگذاری اولیه جدول
 
     setInterval(function() {
         updateLogTable(); // برای به روزرسانی جدول هر ۵ ثانیه
     }, 5000);
 
-
+  
+    
 });
 
 function updateLogTable() {
+  
+  document.getElementById("status_ping").innerText="";
+
     fetch('logs.log')
         .then(response => {
             if (!response.ok) {
@@ -74,20 +161,11 @@ function updateLogTable() {
             return response.text();
         })
         .then(data => {
-            var logContent = data.split("\n");
-            var tableBody = $('#logTable tbody');
-            tableBody.empty();
-
-            logContent.forEach(function(log, index) {
-                var row = $('<tr>');
-                row.append($('<td>').text(index + 1));
-                row.append($('<td>').text(log));
-
-                tableBody.append(row);
-            });
+          var regexx= /\[(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{6}\+\d{2}:\d{2})\]\s(.+): (.+)/;
+           getData(data,'#logTable',regexx);
         })
         .catch(error => {
-            console.error(error); // جهت نمایش خطا در کنسول مرورگر
+            // console.error(error); // جهت نمایش خطا در کنسول مرورگر
         });
 }
 </script>
